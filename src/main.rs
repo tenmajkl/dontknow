e98611f9369e670e32d47d9d4f954b82d8de1ef5
TENMAJKL <silder.hologram@gmail.com>
2024-10-13 00:02:08 +0000

    adds deleting, visible cursor, cleared screen and newlines

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::io::{stdin, stdout, Read, Write};
 
-use termios::{tcsetattr, Termios, ECHO, ICANON, TCSANOW};
+use termios::{tcsetattr, Termios, ECHO, ECHOE, ECHOK, ECHONL, ICANON, IEXTEN, ISIG, OPOST, TCSANOW};
 
+#[derive(PartialEq)]
 enum Mode {
     Normal,
     Insert,
@@ -15,9 +16,16 @@ enum State {
     Error(String),
 }
 
+struct Cursor {
+    x: usize,
+    y: usize,
+}
+
 struct Editor {
     mode: Mode,
     buffer: String,
+    text: Vec<String>,
+    cursor: Cursor,
 }
 
 impl Editor {
@@ -25,28 +33,63 @@ impl Editor {
         let input = *buffer.get(0).unwrap() as char;
         
         let (state, mode) = match self.mode {
-            Mode::Insert => Editor::handle_insert(input),
+            Mode::Insert => self.handle_insert(input),
             Mode::Normal => Editor::handle_normal(input),
             Mode::Command => self.handle_command(input),
         };
 
         self.mode = mode;
 
+        println!("{}", self.text.len());
+        print!("\x1b[2J\x1b[0;0H");
+        let mut x = 0;
+        let mut y = 0;
+        for line in &self.text {
+            for c in line.split("") {
+                print!("{}", c);
+                if self.mode == Mode::Insert && x == self.cursor.x && y == self.cursor.y {
+                    print!("│");
+                }
+                x += 1;
+            }
+            print!("\n");
+            y += 1;
+            x = 0;
+        }
+        stdout().lock().flush().unwrap();
         return state;
     }
 
-    fn handle_insert(input: char) -> (State, Mode) {
+    fn handle_insert(&mut self, input: char) -> (State, Mode) {
         if input as u8 == 27 {
             return (State::Continue, Mode::Normal);
         }
 
         if input as u8 == 127 {
-            // tbd visualize whole text field as matrix which then would be operated
+            self.text.get_mut(self.cursor.y).unwrap().pop().take();
+            if self.cursor.x != 0 {
+                self.cursor.x -= 1;
+            } else if self.cursor.y != 0 {
+                self.cursor.y -= 1;
+                self.cursor.x = self.text.get(self.cursor.y).unwrap().len();
+
+                // todo better cursor working 
+            }
             return (State::Continue, Mode::Insert);
         }
 
-        print!("{}", input);
-        stdout().lock().flush().unwrap();
+        if input == '\n' {
+            self.cursor.y += 1;
+            self.cursor.x = 0;
+            self.text.push(String::new());
+            return (State::Continue, Mode::Insert);
+        }
+
+        self.text.get_mut(self.cursor.y)
+            .unwrap()
+            .push(input);
+
+        self.cursor.x += 1;
         (State::Continue, Mode::Insert)
     }
 
@@ -75,13 +118,15 @@ fn main() {
     let old_termios = Termios::from_fd(0).unwrap();
     let mut termios = old_termios.clone();
 
-    termios.c_lflag &= !(ICANON | ECHO);
+    termios.c_lflag &= !(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ISIG | IEXTEN);
     tcsetattr(0, TCSANOW, &mut termios).unwrap();
     let mut buff = [0;1];
     let mut input = stdin();
     let mut editor = Editor {
         mode: Mode::Normal,
         buffer: String::new(),
+        text: vec!["".to_string()],
+        cursor: Cursor { x: 0, y: 0 },
     };
     loop {
         input.read_exact(&mut buff).unwrap(); 
